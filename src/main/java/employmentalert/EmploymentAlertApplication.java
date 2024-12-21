787875ba4e0fb1e49c0077f992ae88e305c691a5
o-tao <dhtkdals369@gmail.com>
2024-12-22 00:37:29 +0900

    프로젝트 생성

---
+++ src/main/java/employmentalert/EmploymentAlertApplication.java
@@ -0,0 +1,13 @@
+package employmentalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploymentAlertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/employmentalert/EmploymentAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package employmentalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentAlertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
